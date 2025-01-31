1e53b107f41fd58ec7bb50f16b71bcb2a4516e7b
Lux Ren <115652343+Disasterlll@users.noreply.github.com>
2025-01-31 16:13:41 -0500

    update item description and image path

--- EcommerceWebApp/EcommerceWebApp/Models/Item.cs
+++ EcommerceWebApp/EcommerceWebApp/Models/Item.cs
@@ -7,15 +7,19 @@ namespace EcommerceWebApp.Models
         public string ItemName { get; set; }
         public int Quantity { get; set; }
         public float ItemPrice { get; set; }
+        public string Description { get; set; }
 
+        public string ImagePath { get; set; }
         public Item() { }
 
-        public Item(int itemID, string itemName, int quantity, float itemPrice)
+        public Item(int itemID, string itemName, int quantity, float itemPrice, string description, string imagePath)
         {
             ItemID = itemID;
             ItemName = itemName;
             Quantity = quantity;
             ItemPrice = itemPrice;
+            Description=description;
+            ImagePath=imagePath;   
         }
     }
 }
