9d99e2abef87004668c2d51fd324d9521067825f
Lux Ren <115652343+Disasterlll@users.noreply.github.com>
2025-02-19 14:27:06 -0500

    Update Item.cs

--- EcommerceWebApp/EcommerceWebApp/Models/Item.cs
+++ EcommerceWebApp/EcommerceWebApp/Models/Item.cs
@@ -6,15 +6,17 @@ namespace EcommerceWebApp.Models
         public string ItemName { get; set; }
         public int Quantity { get; set; }
         public float ItemPrice { get; set; }
+        public string Description { get; set; }
 
         public Item() { }
 
-        public Item(int itemID, string itemName, int quantity, float itemPrice)
+        public Item(int itemID, string itemName, int quantity, float itemPrice, string descrption)
         {
             ItemID = itemID;
             ItemName = itemName;
             Quantity = quantity;
             ItemPrice = itemPrice;
+            Description = descrption;
         }
     }
 }
